f3433da378c9d6448ef928107c4c7779c93f2d47
AnnaXH <102370569+AnnaXH@users.noreply.github.com>
2022-03-25 14:43:38 +0000

    default content; compability to new api
    - allowing defining default content in the menu, also added a button to allowing using initial content from a file (not active yet)
    - changes to allow the code to compile : the markdownView class has changed since the newer release so that the previous code won't compile with the new API

---
+++ settings.ts
@@ -0,0 +1,45 @@
+import { DAY_PLANNER_DEFAULT_CONTENT } from './constants';
+
+
+export class DayPlannerSettings {
+  customFolder: string = 'Day Planners';
+  mode: DayPlannerMode = DayPlannerMode.File;
+  useTemplateFile: boolean = false;
+  initialTemplate: string = DAY_PLANNER_DEFAULT_CONTENT;
+  mermaid: boolean = false;
+  notesToDates: NoteForDate[] = [];
+  completePastItems: boolean = true;
+  circularProgress: boolean = false;
+  nowAndNextInStatusBar: boolean = false;
+  showTaskNotification: boolean = false
+  timelineZoomLevel: number = 4;
+  timelineIcon: string = 'calendar-with-checkmark'
+  breakLabel: string = "BREAK";
+  endLabel: string = "END";
+}
+
+export class NoteForDate {
+  notePath: string;
+  date: string;
+
+  constructor(notePath: string, date:string){
+    this.notePath = notePath;
+    this.date = date;
+  }
+}
+
+export class NoteForDateQuery {
+  exists(source: NoteForDate[]): boolean {
+    return this.active(source) !== undefined;
+  }
+
+  active(source: NoteForDate[]): NoteForDate{
+    const now = new Date().toDateString();
+    return source && source.filter(ntd => ntd.date === now)[0];
+  }
+}
+  
+export enum DayPlannerMode {
+  File,
+  Command
+}
